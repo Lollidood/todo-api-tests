97d5f65c6ff297eee0844a1bb20e29acba7fe7f8
Nomin-Erdene <nomio2407@yahoo.com>
2026-03-23 15:19:30 +0300

    Create test_todo.py

---
+++ test_todo.py
@@ -0,0 +1,15 @@
+import requests
+
+BASE_URL = "https://jsonplaceholder.typicode.com"
+
+def test_get_todos():
+    response = requests.get(f"{BASE_URL}/todos")
+    
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_get_single_todo():
+    response = requests.get(f"{BASE_URL}/todos/1")
+    
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
